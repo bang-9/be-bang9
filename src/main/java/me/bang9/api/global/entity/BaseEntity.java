791bf8a924395bb0e4da4cdcae6c6f7af2e75743
Poby <smg981024@gmail.com>
2025-07-25 21:00:23 +0900

    fix(base-entity): rename BaseJpaEntity -> BaseEntity

--- src/main/java/me/bang9/api/global/entity/BaseEntity.java
+++ src/main/java/me/bang9/api/global/entity/BaseEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
-public abstract class BaseJpaEntity {
+public abstract class BaseEntity {
 
     @Column(nullable = false)
     protected Boolean status = Boolean.TRUE;
